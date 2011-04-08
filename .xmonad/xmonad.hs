13e587ce8bbc1930bd203649a612a128df54389b
Aleksey Rybalkin <alekseyrybalkin@gmail.com>
2011-04-08 12:44:15 +0400

    add my keybindings to XMonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,12 +1,19 @@
 import XMonad
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ICCCMFocus
+import XMonad.Util.EZConfig
+import XMonad.Util.Run(spawnPipe)
 
-main = xmonad defaultConfig
+main = xmonad $ defaultConfig
     { terminal = "urxvt"
     , startupHook = setWMName "LG3D"
     , logHook = takeTopFocus
     , borderWidth = 1
     , normalBorderColor = "#555555"
     , focusedBorderColor = "#ffa522"
-    }
+    } `additionalKeys` myKeys
+
+myKeys =
+    [ ((mod4Mask, xK_l), spawn "xlock"),
+      ((controlMask .|. mod1Mask, xK_Delete), spawn "sudo reboot")
+    ]
