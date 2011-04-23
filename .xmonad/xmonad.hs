a886f820fe25f7bc49b165977a2746d60bfb0b18
Aleksey Rybalkin <alekseyrybalkin@gmail.com>
2011-04-24 01:37:46 +0400

    use smart borders in XMonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,10 +3,12 @@ import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ICCCMFocus
 import XMonad.Util.EZConfig
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.NoBorders
 
 main = xmonad $ defaultConfig
     { terminal = "urxvt"
     , startupHook = setWMName "LG3D"
+    , layoutHook = smartBorders $ layoutHook defaultConfig
     , logHook = takeTopFocus
     , borderWidth = 1
     , normalBorderColor = "#555555"
