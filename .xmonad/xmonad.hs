97752675a9e613fd068dd9fa47f42cfaa3749276
Aleksey Rybalkin <aleksey@rybalkin.org>
2011-11-19 05:48:11 +0400

    make amixer quiet

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -19,8 +19,8 @@ myKeys =
     [
       ((mod4Mask, xK_l), spawn "xlock"),
       ((mod4Mask, xK_p), spawn "mpc-toggle"),
-      ((mod4Mask, xK_equal), spawn "amixer -c 0 set Master 1dB+"),
-      ((mod4Mask, xK_minus), spawn "amixer -c 0 set Master 1dB-"),
+      ((mod4Mask, xK_equal), spawn "amixer -q -c 0 set Master 1dB+"),
+      ((mod4Mask, xK_minus), spawn "amixer -q -c 0 set Master 1dB-"),
       ((mod1Mask, xK_p), spawn "dmenu_run"),
       ((controlMask .|. mod1Mask, xK_Delete), spawn "sudo poweroff")
     ]
