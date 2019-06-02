d3cf01a5ab70005ec94eeeab00d12fc9c5efcf2d
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-06-02 14:03:15 +0300

    pythonstartup.py: improve xdir

--- .config/pythonstartup.py
+++ .config/pythonstartup.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2014-2019 Aleksey Rybalkin
 # SPDX-License-Identifier: GPL-3.0+
+import itertools
+import shutil
 import sys
 
 
@@ -8,7 +10,19 @@ sys.__interactivehook__ = lambda: None
 
 
 # slightly better `dir`
-def xdir(obj):
-    for i in dir(obj):
-        if not i.startswith('_'):
-            print(i)
+def xdir(obj=None):
+    attr_list = dir(obj) if obj is not None else globals()
+    attrs = [attr for attr in attr_list if not attr.startswith('_')]
+    if not attrs:
+        return
+
+    sep = 3
+    term_len = shutil.get_terminal_size().columns
+    max_len = max(len(attr) for attr in attrs)
+    columns = term_len // (max_len + sep)
+    rows = (len(attrs) // columns) + (1 if len(attrs) % columns else 0)
+
+    batches = [attrs[x * rows:(x + 1) * rows] for x in range(columns)]
+
+    for line in itertools.zip_longest(*batches, fillvalue=''):
+        print((' ' * sep).join(['{:>' + str(max_len) + '}'] * columns).format(*line))
